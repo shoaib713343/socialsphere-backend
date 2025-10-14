f329b490788e99120016160403ef579d1aaa788b
Shoaib <shoaib713343@gmail.com>
2025-10-14 17:54:12 +0530

    refactor(ci): Use Jest setup file for env variables

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 60000,
+  setupFilesAfterEnv: ['./src/__tests__/setup.ts'],
   testMatch: ['**/*.test.ts'],
   verbose: true,
   forceExit: true,
